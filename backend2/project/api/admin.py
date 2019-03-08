414a1c40a2dc92e57aca5ee5b3c16bdc3b0d878b
starei <jimstaras@gmail.com>
2019-03-08 11:18:29 +0200

    deleted wrong backend

--- backend2/project/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Observation, Company, Price
-
-admin.site.register(Observation)
-admin.site.register(Company)
-admin.site.register(Price)
